02ffe281160215f87c6edc8f94d0656d4d629834
Raj Agrawal <r.agrawal@berkeley.edu>
2015-11-11 19:04:05 -0800

    add visuals

---
+++ code/scripts/dataprep_script.py
@@ -0,0 +1,9 @@
+
+#Import standard libraries
+import numpy as np
+import nibabel as nb
+import matplotlib.pyplot as plt
+import data_loading as dl
+import plotting_fmri as plt_fmri
+import save_files as sv
+
