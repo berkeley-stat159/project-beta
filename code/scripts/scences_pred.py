1b1f5aa9cef81c6bef3e6fa64543db62937c7cdf
Raj Agrawal <r.agrawal@berkeley.edu>
2015-11-24 03:41:56 -0800

    added some skeleton functions to scenes_pred script

--- code/scripts/scences_pred.py
+++ code/scripts/scences_pred.py
@@ -1,7 +1,9 @@
-
+#FIX ALL PATHS AND CHANGE FILE NAMES SINCE NOT CORRECT
+#PROBABLY WANT TO DO THIS ON CLEANED DATA  
 
 #Import standard libraries
 import numpy as np
+import pandas as pd
 import nibabel as nb
 import matplotlib.pyplot as plt
 import data_loading as dl
@@ -15,7 +17,11 @@ files = ['task001_run001.bold_dico.nii', 'task001_run002.bold_dico.nii',
          'task001_run005.bold_dico.nii', 'task001_run006.bold.nii'
          'task001_run007.bold.nii', 'task001_run008.bold.nii']
 
-scenes = 'scene_times_nums.csv' 
+scenes = 'scene_times_nums.csv' #FIX
+scenes = pd.read_csv('scene_times_nums.csv') 
+scenes = scenes.values #Now just numpy array
+
+combined_runs = combine_run_arrays((run1, run2, run3, )) #FIX AND ADD runi
 
 #We will first check if there are any noticable differences between scenes
 #occuring in 'Gump house/room/etc.' vs. all other scenes 
@@ -23,7 +29,15 @@ scenes = 'scene_times_nums.csv'
 #In this simple example, we let 1 denote a Gump scene and 0 for all other 
 #scenes 
 
-#FIX: PUT THESE FUNCTIONS IN UTILS 
+#FIX: PUT THESE FUNCTIONS SOMEWHERE IN UTILS
+
+###################################################################
+def combine_run_arrays(run_array_lst):
+	return np.concatenate(array_lst, axis = 3)
+
+def train_test_split(vox_by_time, train_times):
+
+def get_scenes(ids):
 
 def other_scene_ids(remove_ids):
 	""" Return list of ids that do not contain any ids present in remove_ids. 
@@ -42,8 +56,13 @@ def other_scene_ids(remove_ids):
 	other_ids = [i for i in all_ids if i not in remove_ids]
     return other_ids
 
+def get_voxel_weight(arr4d, voxel):
+	
+###################################################################
+
 GUMP_SCENES_IDS = [38, 40, 41, 42] #factor ids of Gump scenes
-OTHER_SCENES = 
+other_scenes = other_scene_ids(GUMP_SCENES_IDS)
+
 
 
 #Next we will look at political scenes vs. non-political scenes 
@@ -51,3 +70,8 @@ OTHER_SCENES =
 #Here we investigate outdoors vs. indoor scenes 
 
 #The following compares the 6 major scenes in the film 
+
+
+
+
+
