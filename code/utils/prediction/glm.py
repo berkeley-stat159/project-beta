0b4a604a4dfefbafa6cf4c168bb54c1f23167fd8
Yucheng Dong <dongyucheng@berkeley.edu>
2015-11-30 18:09:12 -0800

    add glm.py documentation

--- code/utils/prediction/glm.py
+++ code/utils/prediction/glm.py
@@ -3,14 +3,36 @@ from sklearn import linear_model as lm
 import numpy.linalg as npl
 import matplotlib.pyplot as plt
 
+""" unclear about this function, need further discussion
+
 def sk_regression(data_4d, convolved, model):
 	num_vols = data_4d.shape[-1]
 	assert(len(convolved) == num_vols)
-	X = np.ones(num_vols, 2)
+	X = np.ones((num_vols, 2))
 	X[:, 1] = convolved
 	vox_by_time = np.reshape(data_4d, (-1, num_vols))
 
+"""
+
 def glm(data_4d, convolved):
+    """
+    Return a tuple of the estimated coefficients in 4 dimensions and 
+    the design matrix. 
+    
+    Parameters
+    ----------
+    data_4d: numpy array of 4 dimensions 
+        The image data of one subject
+    conv: numpy array of 1 dimension
+        The convolved time course
+    Note that the fourth dimension of `data_4d` (time or the number 
+    of volumes) must be the same as the length of `convolved`. 
+    
+    Returns
+    -------
+    glm_results : tuple
+        Estimated coefficients in 4 dimensions and the design matrix.
+    """
 	num_vols = data_4d.shape[-1]
 	assert(len(convolved) == num_vols)
 	design = np.ones((num_vols, 2))
@@ -21,8 +43,18 @@ def glm(data_4d, convolved):
 	return (X, betas_4d)
 
 def scale_design_mtx(X):
-    """utility to scale the design matrix for display
+    """ 
+    Return a scaled design matrix for display
+    
+    Parameters
+    ----------
+    X: Design Matrix
+    
+    Returns
+    -------
+    Xs: a scaled design matrix 
 
+    Estimated coefficients in 4 dimensions and the design matrix.
     This scales the columns to their own range so we can see the variations
     across the column for all the columns, regardless of the scaling of the
     column.
