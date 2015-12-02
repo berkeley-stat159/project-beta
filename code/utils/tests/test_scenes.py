c15e440bdfdb9519e5efe5daa8fad0d7d7395725
Yucheng Dong <dongyucheng@berkeley.edu>
2015-12-02 14:39:21 -0800

    fix python3 problems v3

--- code/utils/tests/test_scenes.py
+++ code/utils/tests/test_scenes.py
@@ -464,8 +464,14 @@ def get_training_samples(samples):
 
  
 def test_training_samples():
-    r5 = np.array([128, 129, 130, 131, 132, 133, 134, 135, 136, 137])
-    f5= get_training_samples(samp_gump).values()[0]
+    r5 = [np.array([128, 129, 130, 131, 132, 133, 134, 135, 136, 137]),
+ np.array([148, 149, 150, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173,
+        174, 175, 176, 177, 178, 179, 180]),
+ np.array([185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197,
+        198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 250, 251,
+        252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264,
+        265, 266, 267, 268])]
+    f5= get_training_samples(samp_gump).values()
     assert_almost_equal(f5,r5)
     
 def make_label_by_time(sing_samp):
