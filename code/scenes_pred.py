0b9890232ad5b25fea34c5e39774ce829f8a5653
Raj Agrawal <r.agrawal@berkeley.edu>
2015-12-02 23:45:11 -0800

    finally works (up to analysis sec

--- code/scenes_pred.py
+++ code/scenes_pred.py
@@ -1,9 +1,8 @@
 
 """ The following script will analyze the scenes data. Specifically, it will:
 
-* Use PCA to reduce data/noise for fitting 
 * Try to find patterns between neural responses and scenes   
-* Use regression, SVM, and KNN to link these together
+* Use kmeans and KNN to link these together
 * Predict scenes based on BOLD activity 
 * Test the performance of the classification through cross-validation  
 
@@ -25,32 +24,46 @@ from sklearn.decomposition import PCA
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier as KNN 
 from sklearn.cluster import KMeans
-from scipy.spatial.distance import hamming 
                                             
-
 #All file strings corresponding to BOLD data for subject 4 
 files = ['../data/task001_run001.bold_dico.nii', '../data/task001_run002.bold_dico.nii', 
          '../data/task001_run003.bold_dico.nii', '../data/task001_run004.bold_dico.nii', 
          '../data/task001_run005.bold_dico.nii', '../data/task001_run006.bold_dico.nii',
          '../data/task001_run007.bold_dico.nii', '../data/task001_run008.bold_dico.nii']
 
+#Use only 6810 of the voxels (choose most variance-stable voxels for further analysis)
+SUBSET_VOXELS_PATH = '../brain_mask/mask6810.npy'
+mask = np.load(SUBSET_VOXELS_PATH) #mask 
+VOXEL_INDCS = np.where(mask == True)[0]
+
+unraveled = []
+for one_d_pos in VOXEL_INDCS:
+    three_d_pos = np.unravel_index(one_d_pos, (160, 160, 36))
+    unraveled.append(three_d_pos)
+unraveled = np.array(unraveled)
+
+first_comp = unraveled[:,0]
+sec_comp = unraveled[:,1]
+third_comp = unraveled[:,2]
+
 #Load in data 
 all_data = []
 for index, filename in enumerate(files):
     new_data = dl.load_data(filename) #load_data function drops first 4 for us
+    new_data = new_data[first_comp, sec_comp, third_comp, :] #vox by time array 2d
     num_vols = new_data.shape[-1]
     if index != 0 and index != 7:
         new_num_vols = num_vols - 4   
-        new_data = new_data[:,:,:,:new_num_vols] #Drop last 4 volumes for middle runs    
-    print(new_data.shape[-1])
+        new_data = new_data[:,:new_num_vols] #Drop last 4 volumes for middle runs    
     all_data.append(new_data)
 
 scenes_path = '../data/scene_times_nums.csv'
 scenes = pd.read_csv(scenes_path, header = None) 
 scenes = scenes.values #Now just a numpy array
 
-combined_runs = sn.combine_run_arrays(all_data) 
-combined_runs = combined_runs[:,:,:,9:] #First 17 seconds are credits/no scene id so drop
+combined_runs = np.concatenate(all_data, axis = 1) 
+combined_runs = combined_runs[:,9:] #First 17 seconds are credits/no scene id so drop
+all_data = [] #Dont need to store this anymore
 
 TR = 2
 NUM_VOLUMES = combined_runs.shape[-1] #3459 
@@ -73,9 +86,13 @@ for scan_time in SCAN_TIMES:
 
 factor_grid = np.array(factor_grid) #Convert to np array for future analysis
 
-###################################################################
+############K-means Analysis ##################################
 #Set up training and test set for Gump 
 GUMP_SCENES_IDS = [38, 40, 41, 42] #factor ids of Gump scenes
+MILITARY_IDS = [52, 62, 77, 78, 80, 81, 82, 83]
+SCHOOL = [22,43, 67, 61, 69]
+SAVANNA = [66]
+POLITICAL = 
 other_scenes = other_scene_ids(GUMP_SCENES_IDS)
 
 samp_gump, miss_gump = gen_sample_by_factors(GUMP_SCENES_IDS, factor_grid, True)
